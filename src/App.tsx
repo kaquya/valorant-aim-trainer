21d2c4ea9c6dfff2559ba18f2dade7864b13989d
Jeffrey Blanco <jeffrey.blanco@band.ch>
2026-05-12 07:35:58 +0200

    Initial Vite React setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Valorant Aim Trainer</h1>
+      <p>Sensitivity finder and browser-based aim training.</p>
+    </main>
+  );
+}
+
+export default App;
